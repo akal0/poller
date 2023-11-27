b6aaf193dd4331570a462789f8aa331b6fe66911
Abdallah Kalenga <abdallahkalenga1689@gmail.com>
2023-11-27 02:03:30 +0000

    .

--- src/app/[poll_id]/page.tsx
+++ src/app/[poll_id]/page.tsx
@@ -4,45 +4,41 @@ import Votes from "@/components/poll/Votes"
 
 const PollPage = async ({ params }: { params: { poll_id: string } }) => {
 	const pollId = params.poll_id
-	try {
-		// Send a GET request to PartyKit room
+	// Send a GET request to PartyKit room
 
-		const getPoll = async () => {
-			"use server"
+	const getPoll = async () => {
+		"use server"
 
-			const req = await fetch(`${PARTYKIT_URL}/party/${pollId}`, {
-				method: "GET",
-				next: {
-					revalidate: 0,
-				},
-			})
+		const req = await fetch(`${PARTYKIT_URL}/party/${pollId}`, {
+			method: "GET",
+			next: {
+				revalidate: 0,
+			},
+		})
 
-			const poll = (await req.json()) as Poll
-			return poll
-		}
-
-		const poll = await getPoll()
-
-		// Feed the data into poll var
-
-		return (
-			<div className="flex flex-col justify-center space-y-6 min-w-[50%] mx-auto min-h-[calc(100vh-16rem)] md:min-h-[calc(100vh-8rem)] px-8">
-				<h1 className="text-xl md:text-3xl font-semibold text-center">
-					{poll?.title}
-				</h1>
-
-				{poll && (
-					<Votes
-						id={pollId}
-						options={poll.options}
-						initialVotes={poll.votes}
-					/>
-				)}
-			</div>
-		)
-	} catch (err) {
-		console.log(err)
+		const poll = (await req.json()) as Poll
+		return poll
 	}
+
+	const poll = await getPoll()
+
+	// Feed the data into poll var
+
+	return (
+		<div className="flex flex-col justify-center space-y-6 min-w-[50%] mx-auto min-h-[calc(100vh-16rem)] md:min-h-[calc(100vh-8rem)] px-8">
+			<h1 className="text-xl md:text-3xl font-semibold text-center">
+				{poll?.title}
+			</h1>
+
+			{poll && (
+				<Votes
+					id={pollId}
+					options={poll.options}
+					initialVotes={poll.votes}
+				/>
+			)}
+		</div>
+	)
 }
 
 export default PollPage
