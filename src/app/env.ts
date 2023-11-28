1d224ab2f6a1958ff56797bbdc3c9df25d5c9886
Abdallah Kalenga <abdallahkalenga1689@gmail.com>
2023-11-28 13:58:40 +0000

    .

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,6 @@
 export const PARTYKIT_HOST =
-	process.env.NEXT_PUBLIC_PARTYKIT_HOST ?? "127.0.0.1:1999"
+	process.env.NEXT_PUBLIC_PARTYKIT_HOST ??
+	"https://poller.akal0.partykit.dev/"
 
 export const PROTOCOL = PARTYKIT_HOST.startsWith("127.0.0.1") ? "http" : "https"
 
